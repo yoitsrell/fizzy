60b8d5357a47ac68ee3f5882fe397d1d69badd30
Tyrell Cotton <tyrell.cotton@codeimmersives.com>
2020-02-05 09:56:30 -0500

    initial

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-function fizzy(num) {
-
-}
+function fizzy(num) 
+  {
+  if (num % 3 ===0)
+  {
+  return "Fizz"}
+  else if (num %5 ===0)
+  {
+  return 'Buzz'}
+  else if (num % 3 === 0 && num %5 ===0)
+  {
+  return "FizzBuzz"}
+  else if(num){
+    return num
+  }
+  }
 
 module.exports = {
   fizzy,
 }
+
+console.log(fizzy())
